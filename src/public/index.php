c26868400c5ba1bb3725611ada645f93d651f095
about69feminists <maag.flo@gmail.com>
2023-02-07 19:11:25 +0100

    classes created and tested

--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,13 @@
 <?php
-phpinfo();
 
-?>
-<pre>
-    <?php print_r($_SERVER); ?>
-</pre>
+declare(strict_types=1);
+
+require_once '../Transaction.php';
+
+//Classes & Objects
+$amount = (new Transaction(100, 'Tx 1'))
+    ->addTax(7)
+    ->addDiscount(10)
+    ->getAmount();
+
+var_dump($amount);
