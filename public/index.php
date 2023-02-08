416ed06a2a03bd0c73a08c50352ce86f945cac53
about69feminists <maag.flo@gmail.com>
2023-02-08 21:34:22 +0100

    encapsulation added

--- public/index.php
+++ public/index.php
@@ -6,12 +6,12 @@ use \App\PaymentGateway\Paddle\Transaction;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-//$tx = new Transaction(25, 'Tx');
-//$tx = new Transaction(25, 'Tx');
-//$tx = new Transaction(25, 'Tx');
-//$tx = new Transaction(25, 'Tx');
-//$tx = new Transaction(25, 'Tx');
-//$tx = new Transaction(25, 'Tx');
+//encapsulation = access modifiers + setter & getter
+//zb. property to private and use getter/setter do access it
 
+$tx = new Transaction(25);
 
-var_dump(Transaction::getCount());
+//abstraction: hide the implementation
+//this process function should not care what happens afterwards
+//the code should still work if changes are made somewhere else
+$tx->process();
--- src/app/PaymentGateway/Paddle/Transaction.php
+++ src/app/PaymentGateway/Paddle/Transaction.php
@@ -6,21 +6,19 @@ namespace App\PaymentGateway\Paddle;
 
 class Transaction
 {
-    private static int $count = 0;
+    private float $amount;
 
-    public function __construct(
-        public float $amount,
-        public string $description
-    ) {
-        self::$count++;
-    }
-    public static function getCount(): int
-    {
-        return self::$count;
+    public function __construct(float $amount) {
+        $this->amount = $amount;
     }
 
     public function process(): void
     {
-        echo "Processing Paddle Transaction";
+        echo "Processing $" . $this->amount . " Transaction";
+    }
+
+    public function getAmount(): float
+    {
+        return $this->amount;
     }
 }
