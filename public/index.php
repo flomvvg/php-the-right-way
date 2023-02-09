184c7320c9b58d7fe5091f4d7eb0d1affb26b221
maagcyb <maag@cyberlink.ch>
2023-02-09 16:24:22 +0100

    late static binding tested

--- public/index.php
+++ public/index.php
@@ -4,7 +4,23 @@ declare(strict_types=1);
 
 
 require __DIR__ . '/../vendor/autoload.php';
+//runtime: the interpreter is reading live information
+//compiled: the interpreter checks all the code, compiles it and runs it
 
-$invoice = new \App\Invoice();
+//late binding: property-binding happens at runtime
 
-var_dump($invoice);
+//$a = new \App\ClassA();
+//$b = new \App\ClassB();
+
+//echo $a->getName();
+//echo $b->getName();
+
+//self keyword always references its own class which the code is executed.
+//= early binding
+
+//static keyword uses late binding
+//echo \App\ClassA::getName();
+//echo \App\ClassB::getName();
+
+
+var_dump(\App\ClassB::make());
