bc3230d8beb9c206f1da142ef06bf35c7b6d96d1
maagcyb <maag@cyberlink.ch>
2023-02-10 15:26:42 +0100

    object cloned

--- public/index.php
+++ public/index.php
@@ -5,4 +5,10 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-//docblocks not really needed anymore with type hinting
+$invoice1 = new \App\Invoice();
+
+//creates a copy of an object with a different memory location
+//in this case invoice 2 is a new object which does not point to invoice 1 but has the same values
+$invoice2 = clone $invoice1;
+
+var_dump($invoice1, $invoice2, \App\Invoice::create());
---
+++ src/app/Invoice.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class Invoice
+{
+    public static function create()
+    {
+        return new static();
+    }
+}
