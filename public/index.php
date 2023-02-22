ae5b9d0e88f3fe2f4781dedcccad5875112459f5
maagcyb <maag@cyberlink.ch>
2023-02-22 14:15:36 +0100

    serialited objects

--- public/index.php
+++ public/index.php
@@ -2,13 +2,32 @@
 
 declare(strict_types=1);
 
-
 require __DIR__ . '/../vendor/autoload.php';
 
-$invoice1 = new \App\Invoice();
+use App\Invoice;
 
-//creates a copy of an object with a different memory location
-//in this case invoice 2 is a new object which does not point to invoice 1 but has the same values
-$invoice2 = clone $invoice1;
+$invoice = new Invoice(25, 'name', 'description', '1234');
 
-var_dump($invoice1, $invoice2, \App\Invoice::create());
+//serialization is converting a value to a string form
+echo serialize(true);
+echo '<br>';
+echo serialize(1);
+echo '<br>';
+echo serialize('hello world');
+echo '<br>';
+echo serialize(2.5);
+echo '<br>';
+echo serialize([1,2,3]);
+echo '<br>';
+var_dump(unserialize(serialize(['a' => 1, 'b' => 2, 'c' => 3])));
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+
+//unserialize creates a new object
+$str = serialize($invoice);
+$invoice2 = unserialize($str);
+
+var_dump($invoice2, $invoice === $invoice2);
--- src/app/Invoice.php
+++ src/app/Invoice.php
@@ -4,8 +4,47 @@ namespace App;
 
 class Invoice
 {
-    public static function create()
+    public function __construct(public float $amount, public string $name, public string $desc, public string $creditCardNr)
     {
-        return new static();
+    }
+
+    public function __sleep(): array
+    {
+        //called before the serialization happens
+
+        //here it will only serialize the amount. This array can only be properties or methods of the object
+        return ['amount'];
+    }
+
+    public function __wakeup(): void
+    {
+        //called after object has been unserialized
+    }
+
+    public function __serialize(): array
+    {
+        //called before serialization, but serialize must return an array that represents the object
+        //can also have more data than just the properties
+
+        //if serialize and sleep methods are defined: serialize will be called instead of sleep
+
+        return [
+            'name' => $this->name,
+            'amount' => $this->amount,
+            'desc' => $this->desc,
+            'creditCardNr' => base64_encode($this->creditCardNr),
+            'foo' => 'bar'
+        ];
+    }
+
+    public function __unserialize(array $data): void
+    {
+        //called after object has been unserialized
+        //gets the data that was serialized. Can restore the data of the object
+
+        $this->amount = $data['amount'];
+        $this->name = $data['name'];
+        $this->desc = $data['desc'];
+        $this->creditCardNr = base64_decode($data['creditCardNr']);
     }
 }
